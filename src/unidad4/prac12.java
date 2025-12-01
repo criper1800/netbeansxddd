28cc661985832fdfc66010203d1edaa5129c3675
Lab3 <Lab3@LAB1-8>
2025-12-01 11:41:00 -0600

    wuea

---
+++ src/unidad4/prac12.java
@@ -0,0 +1,22 @@
+
+package unidad4;
+
+import java.util.Scanner;
+
+
+public class prac12 {
+    public static void main(String[] args) {
+         Scanner leer= new Scanner(System.in);
+    float peso;
+    int zona;
+    float precio;
+        System.out.println("peso por gramos");
+        peso = leer.nextFloat();
+        
+         
+         
+         
+         
+    }
+  
+}
