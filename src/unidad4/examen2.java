615b218cf248f751abcfe9c7007e2ef5ea870ea1
Lab3 <Lab3@LAB1-8>
2025-12-04 11:32:38 -0600

    wuea

---
+++ src/unidad4/examen2.java
@@ -0,0 +1,33 @@
+
+package unidad4;
+
+import java.util.Scanner;
+
+
+public class examen2 {
+    public static void main(String[] args) {
+        Scanner leer= new Scanner(System.in);
+        double hora, th, th2, t3; 
+        System.out.println("cuantas horas");
+        hora = leer.nextDouble();
+        th=2*15;
+        if (hora>0 && hora<3){
+            th2=hora*15;
+            System.out.println("total "+th2);
+        }else if(hora>2 && hora<4){}
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+        
+        
+        
+    
+  
+}
