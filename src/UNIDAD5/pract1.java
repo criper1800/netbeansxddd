fa24817389911aa2f4181d54036333e34be9e5d2
Lab3 <Lab3@LAB1-8>
2025-12-11 09:54:39 -0600

    wuea

---
+++ src/UNIDAD5/pract1.java
@@ -0,0 +1,32 @@
+
+package UNIDAD5;
+
+import java.util.Scanner;
+
+
+public class pract1 {
+public static void main(String[] args) {
+          Scanner leer= new Scanner(System.in);
+
+          
+       int num;  
+       int suma=0;
+    for(int cont = 0; cont< 10; cont++){
+        System.out.println("dame un numero");
+        num= leer.nextInt();
+        suma =suma+num;
+        
+    }    
+    System.out.println("suma: "+suma);       
+          
+          
+          
+          
+          
+          
+          
+          
+          
+    }
+ 
+}
